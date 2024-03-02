a292a880620bcb3e04b60a15e562d9ce62f135ed
AdrianGarciaMartin <agarciamar@edu.tecnocampus.cat>
2024-03-02 23:59:12 +0100

    Selección de item comenzada

--- Project/Assets/Scripts/InventorySystem/ItemSelected.cs
+++ Project/Assets/Scripts/InventorySystem/ItemSelected.cs
@@ -6,6 +6,9 @@ public class ItemSelected : MonoBehaviour //detectar el item sobre el que hago c
     // y pasarle los scripts del sell, buy y consume para que al seleccionar un objeto y luego pulsar algo haga la accion designada
     // y depende de la acción el dinero se modifica, ten en mente eso también 
 {
+    public Inventory _inventory; //maybe hay que crear un segundo inventory (el del shop) y revisar ambos
+    public bool _selected = false;
+
     // Start is called before the first frame update
     void Start()
     {
@@ -15,6 +18,16 @@ public class ItemSelected : MonoBehaviour //detectar el item sobre el que hago c
     // Update is called once per frame
     void Update()
     {
-        
+        Debug.Log(_selected);
+
+        for (int i = 0; i <= _inventory.Length; i++) //out of range exception (maybe en lugar de _inventory.Length mejor poner _inventory.Slots.Length pero para eso hay que hacer que la lista Slots sea publica (no se si afectará en algo al código))
+        {
+            Debug.Log(_inventory.GetSlot(i));
+        }
+    }
+
+    private void OnMouseDown()
+    {
+        _selected = true;
     }
 }
